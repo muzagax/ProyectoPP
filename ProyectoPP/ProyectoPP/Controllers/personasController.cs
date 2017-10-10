35731ecd161603c22f6bfb6eb2623553d3b54d10
b41736 <b41736@ECCI-LBW-106-09>
2017-10-10 10:19:50 -0600

    Modelo persona vista y controlador de persona

---
+++ ProyectoPP/ProyectoPP/Controllers/personasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoPP.Models;
+
+namespace ProyectoPP.Controllers
+{
+    public class personasController : Controller
+    {
+        private patopurificEntities db = new patopurificEntities();
+
+        // GET: personas
+        public ActionResult Index()
+        {
+            return View(db.persona.ToList());
+        }
+
+        // GET: personas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: personas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: personas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombre,apellido1,apellido2,cedula,carne,fechaNac,email,id,genero")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.persona.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: personas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: personas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nombre,apellido1,apellido2,cedula,carne,fechaNac,email,id,genero")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: personas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            persona persona = db.persona.Find(id);
+            db.persona.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
