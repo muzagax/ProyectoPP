d6a83500169e68664bde0ef195cf90f870cb13da
sebastianVUCR <sebastian.vargas.99@gmail.com>
2017-10-10 20:01:22 -0600

    Roles
    Se puso todo lo relacionado con roles como estaba en la versión anterior (hay un error al ingresar a la interfaz de roles)

---
+++ ProyectoPP/ProyectoPP/Controllers/RolesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoPP.Models;
+
+namespace ProyectoPP.Controllers
+{
+    public class RolesController : Controller
+    {
+
+        patopurificEntitiesRoles baseDatos = new patopurificEntitiesRoles();
+        // GET: Roles
+        public ActionResult Index()
+        {
+
+            Roles modelo = new Roles();
+            modelo.ListaRoles = baseDatos.AspNetRoles.ToList();
+            modelo.ListaPermisos = baseDatos.permisos.ToList();
+            return View(modelo);
+        }
+    }
+}
---
+++ ProyectoPP/ProyectoPP/Models/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoPP.Models
+{
+    public class Roles
+    {
+
+        public permisos ModeloPermisos { get; set; }
+        public AspNetRoles ModeloNetRoles { get; set; }
+
+        public List<permisos> ListaPermisos { get; set; }
+
+        public List<AspNetRoles> ListaRoles { get; set; }
+    }
+}
