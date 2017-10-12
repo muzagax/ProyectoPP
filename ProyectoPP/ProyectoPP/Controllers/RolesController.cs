e5dd2795b979a0448efbb28031d2c713e7c541f9
sebastianVUCR <sebastian.vargas.99@gmail.com>
2017-10-11 20:05:26 -0600

    roles
    progreso en cargar datos desde la base a la interfaz

--- ProyectoPP/ProyectoPP/Controllers/RolesController.cs
+++ ProyectoPP/ProyectoPP/Controllers/RolesController.cs
@@ -18,6 +18,19 @@ namespace ProyectoPP.Controllers
             Roles modelo = new Roles();
             modelo.ListaRoles = baseDatos.AspNetRoles.ToList();
             modelo.ListaPermisos = baseDatos.permisos.ToList();
+            modelo.ListaAscociaciones = new List<Roles.Asociaciones>();
+
+           
+
+            foreach (var AspNetRoles in modelo.ListaRoles)
+            {
+
+                foreach (var permisos in AspNetRoles.permisos)
+                {
+                    modelo.ListaAscociaciones.Add(new Roles.Asociaciones(AspNetRoles.Name, permisos.permiso));
+                }
+            }
+
             return View(modelo);
         }
     }
--- ProyectoPP/ProyectoPP/Models/Roles.cs
+++ ProyectoPP/ProyectoPP/Models/Roles.cs
@@ -8,11 +8,24 @@ namespace ProyectoPP.Models
     public class Roles
     {
 
+        public class Asociaciones
+        {
+            public Asociaciones(string rolParam, string permisoParam)
+            {
+                this.rol = rolParam;
+                this.permiso = permisoParam;
+            }
+
+            public string rol { get; set; }
+            string permiso  { get; set; }
+        }
         public permisos ModeloPermisos { get; set; }
         public AspNetRoles ModeloNetRoles { get; set; }
 
         public List<permisos> ListaPermisos { get; set; }
 
         public List<AspNetRoles> ListaRoles { get; set; }
-    }
+
+        public List<Asociaciones>  ListaAscociaciones { get; set; }
+}
 }
