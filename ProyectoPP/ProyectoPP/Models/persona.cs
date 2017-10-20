16d7278d2392a0a0283f3cb4936211495e2aab7f
b41736 <b41736@ECCI-LBW-106-09>
2017-10-20 12:30:04 -0600

    as

--- ProyectoPP/ProyectoPP/Models/persona.cs
+++ ProyectoPP/ProyectoPP/Models/persona.cs
@@ -48,11 +48,27 @@ namespace ProyectoPP.Models
 }
     public class PersonaCrear
     {
+        [Display(Name = "Nombre")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "El nombre solo puede estar compuesto por letras")]
         public string nombre { get; set; }
+
+        [Display(Name = "Primer apellido")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "El primer apellido solo puede estar compuesto por letras")]
         public string apellido1 { get; set; }
+        [Display(Name = "Segundo apellido")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "El segundo apellido solo puede estar compuesto por letras")]
         public string apellido2 { get; set; }
+
+        [RegularExpression(@"^[0-9]{9,9}$", ErrorMessage = "La cédula debe contener 9 numeros")]
+        [Display(Name = "Cédula")]
         public string cedula { get; set; }
+
+        [RegularExpression(@"^[A-Z][0-9]{5,5}$", ErrorMessage = "La primera letra debe estar en mayúscula y contener 5 digitos despues de esta")]
+        [Display(Name = "Carné")]
         public string carne { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9\.\-]+@[a-zA-Z0-9\.\-]+\.[a-z]{1,3}$", ErrorMessage = "No es un formato de correo electronico válido")]
+        [Display(Name = "Correo electrónico")]
         public string email { get; set; }
         public string id { get; set; }
         public string rol { get; set; }
