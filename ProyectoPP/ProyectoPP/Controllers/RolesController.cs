4f8f98a707c0d364afbf09e396ca3c9bd36c6df6
b41176 <b41176@ECCI-LBW-106-07>
2017-10-25 11:26:06 -0600

    Tratando de revisar permisos

--- ProyectoPP/ProyectoPP/Controllers/RolesController.cs
+++ ProyectoPP/ProyectoPP/Controllers/RolesController.cs
@@ -7,7 +7,8 @@ using ProyectoPP.Models;
 using System.Data.Entity;
 
 using System.Threading.Tasks;
-
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace ProyectoPP.Controllers
 {
@@ -16,50 +17,99 @@ namespace ProyectoPP.Controllers
 
         patopurificEntitiesRoles baseDatos = new patopurificEntitiesRoles();
         // GET: Roles
-        public ActionResult RolesView()
+
+        private ProyectoPP.Models.patopurificEntitiesRoles enRoles = new ProyectoPP.Models.patopurificEntitiesRoles();
+
+        private ProyectoPP.Models.patopurificEntitiesUserRoles pEUR = new ProyectoPP.Models.patopurificEntitiesUserRoles();
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private async Task<bool> revisarPermisos(string permiso)
         {
 
-            Roles modelo = new Roles();
-            modelo.ListaRoles = baseDatos.AspNetRoles.ToList();
-            modelo.ListaPermisos = baseDatos.permisos.ToList();
-            modelo.ListaAscociaciones = new List<Roles.Asociaciones>();
-            modelo.ListaGuardar = new List<Roles.GuardarAux>();
+            string userID = System.Web.HttpContext.Current.User.Identity.Name;
 
-            foreach (var AspNetRoles in modelo.ListaRoles)
+            string userIDLargo = UserManager.FindById(userID).Id;
+
+            var userRoles = await _userManager.GetRolesAsync(userID);
+
+            var permisoID = enRoles.permisos.Where(m => m.permiso == permiso).First().id_permiso;
+            var listaRoles = enRoles.AspNetRoles.Where(m => m.permisos.Any(c => c.id_permiso == permisoID)).ToList().Select(n => n.Id);
+
+            //bool userRol = listaRoles.Contains(userRoles);
+            bool userRol = false;
+            foreach (var element in listaRoles)
             {
-
-                foreach (var permisos in AspNetRoles.permisos)
+                foreach (var roles in userRoles)
                 {
-                    modelo.ListaAscociaciones.Add(new Roles.Asociaciones(AspNetRoles.Name, permisos.permiso));
+                    if (element == roles)
+                        userRol = true;
                 }
             }
+            return userRol;
+        }
 
-            foreach(var AspNetRoles in modelo.ListaRoles)
+        public ActionResult RolesView()
+        {
+            Roles modelo = new Roles();
+            if (revisarPermisos("ver accesos").Equals(true))
             {
-                foreach (var permisos in modelo.ListaPermisos)
+                modelo.ListaRoles = baseDatos.AspNetRoles.ToList();
+                modelo.ListaPermisos = baseDatos.permisos.ToList();
+                modelo.ListaAscociaciones = new List<Roles.Asociaciones>();
+                modelo.ListaGuardar = new List<Roles.GuardarAux>();
+
+                foreach (var AspNetRoles in modelo.ListaRoles)
                 {
-                    bool asignado = false;
-                    for(int contador = 0; contador < modelo.ListaAscociaciones.Count();contador++)
+
+                    foreach (var permisos in AspNetRoles.permisos)
                     {
-                        if (AspNetRoles.Name == modelo.ListaAscociaciones.ElementAt(contador).rol)
+                        modelo.ListaAscociaciones.Add(new Roles.Asociaciones(AspNetRoles.Name, permisos.permiso));
+                    }
+                }
+
+                foreach (var AspNetRoles in modelo.ListaRoles)
+                {
+                    foreach (var permisos in modelo.ListaPermisos)
+                    {
+                        bool asignado = false;
+                        for (int contador = 0; contador < modelo.ListaAscociaciones.Count(); contador++)
                         {
-                            if (modelo.ListaAscociaciones.ElementAt(contador).permiso == permisos.permiso)
+                            if (AspNetRoles.Name == modelo.ListaAscociaciones.ElementAt(contador).rol)
                             {
-                                modelo.ListaGuardar.Add(new Roles.GuardarAux(AspNetRoles.Id, permisos.id_permiso, true));
-                                asignado = true;
+                                if (modelo.ListaAscociaciones.ElementAt(contador).permiso == permisos.permiso)
+                                {
+                                    modelo.ListaGuardar.Add(new Roles.GuardarAux(AspNetRoles.Id, permisos.id_permiso, true));
+                                    asignado = true;
+                                }
+                            }
+                            if (contador == modelo.ListaAscociaciones.Count() - 1 && asignado == false)
+                            {
+                                modelo.ListaGuardar.Add(new Roles.GuardarAux(AspNetRoles.Id, permisos.id_permiso, false));
+                                asignado = false;
                             }
                         }
-                        if (contador == modelo.ListaAscociaciones.Count() - 1 && asignado == false)
-                        {
-                            modelo.ListaGuardar.Add(new Roles.GuardarAux(AspNetRoles.Id, permisos.id_permiso, false));
-                            asignado = false;
-                        }
-                    }
-                   
-                }
-            }
 
-            return View(modelo);
+                    }
+                }
+                return View(modelo);
+            }
+            else
+            {
+                return View("Views/Home/Index");
+            }
         }
 
         [HttpPost]
