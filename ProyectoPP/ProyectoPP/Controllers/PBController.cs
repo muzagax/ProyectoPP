fe1946862a667d624803b57f85be1b735d6b5fc7
sebastianVUCR <sebastian.vargas.99@gmail.com>
2017-10-29 16:34:57 -0600

    PB, persona,layout
    se agregó el consultar de PB (en desorden para el profesor) y el link para acceder a la ventana. Se actualizó el modelo y se cambió persona.cs

---
+++ ProyectoPP/ProyectoPP/Controllers/PBController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoPP.Models;
+using System.Data.Entity;
+
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace ProyectoPP.Controllers
+{
+    public class PBController : Controller
+    {
+
+        patopurificEntitiesGeneral bd = new patopurificEntitiesGeneral();
+        // GET: PB
+        public ActionResult ProductBacklogIndex()
+        {
+            ModeloProductBacklog modelo = new ModeloProductBacklog();
+
+
+
+            modelo.ListaPB = bd.historiasDeUsuario.ToList();
+
+            return View(modelo);
+        }
+    }
+}
