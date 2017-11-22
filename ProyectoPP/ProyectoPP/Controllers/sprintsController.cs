414feb84fa2037b77e98d140ecbeb8490b6d3754
a86276 <josemsa1369@gmail.com>
2017-11-22 10:10:47 -0600

    sprint planning

--- ProyectoPP/ProyectoPP/Controllers/sprintsController.cs
+++ ProyectoPP/ProyectoPP/Controllers/sprintsController.cs
@@ -7,12 +7,48 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProyectoPP.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace ProyectoPP.Controllers
 {
     public class sprintsController : Controller
     {
         private patopurificEntitiesGeneral db = new patopurificEntitiesGeneral();
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private async Task<bool> revisarPermisos(string permiso)
+        {
+
+            string userName = System.Web.HttpContext.Current.User.Identity.Name;
+            var user = UserManager.FindByName(userName);
+            var rol = user.Roles.SingleOrDefault().RoleId;
+
+            var permisoID = db.permisos.Where(m => m.permiso == permiso).First().id_permiso;
+            var listaRoles = db.AspNetRoles.Where(m => m.permisos.Any(c => c.id_permiso == permisoID)).ToList().Select(n => n.Id);
+
+            bool userRol = false;
+            foreach (var element in listaRoles)
+            {
+                if (element == rol)
+                    userRol = true;
+            }
+            return userRol;
+        }
 
         // GET: sprints
         public ActionResult Index()
@@ -27,9 +63,32 @@ namespace ProyectoPP.Controllers
         {
             sprint sprint1 = new sprint();
 
-            ViewBag.Proyecto = new SelectList(db.proyecto, "id", "nombre");
-            ViewBag.Sprint = new SelectList(db.sprint, "id", "nombre");
-            ViewBag.HU = db.historiasDeUsuario.ToList();
+            if (revisarPermisos("Ver proyecto").Result) // Si el usuario no es estudiante
+            {
+
+                // Seleccion para el dropdown de proyectos. Carga todos los proyectos que hay
+                ViewBag.Proyecto = new SelectList(db.proyecto, "id", "nombre");
+
+                // El dropdown de sprints queda en blanco porque no sabemos aún cuál proyecto se va a seleccionar. Para esto solamente busco los sprints que id ""
+                ViewBag.Sprint = new SelectList(db.proyecto.Where(x => x.id == ""), "id", "nombre");
+                
+                // Repito el proceso con las HU, busco las que tengan ID ""
+                ViewBag.HU = db.historiasDeUsuario.Where(x => x.proyectoId == "").ToList();
+            }
+
+            else
+            {
+                var idproyecto = db.persona.Where(m => m.cedula == System.Web.HttpContext.Current.User.Identity.Name).First().IdProyecto;
+                
+                // Seleccion para el dropdown de proyectos. Carga solo el proyecto donde participa el estudiante
+                ViewBag.Proyecto = new SelectList(db.proyecto.Where(x => x.id == idproyecto), "id", "nombre");
+
+                // Seleccion para el dropdown de sprints. Carga todos los sprints que hay asociados al proyecto seleccionado
+                ViewBag.Sprint = new SelectList(db.sprint.Where(x => x.proyectoId == idproyecto), "id", "nombre");
+
+                ViewBag.HU = db.historiasDeUsuario.Where(x => x.proyectoId == idproyecto).ToList();
+
+            }
 
             return View(sprint1);
         }
